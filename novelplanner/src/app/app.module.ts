bad5155ef3f1cd886384d830f1093bd6cf5f61cf
Michele de Meester <demeestermichele@gmail.com>
2022-06-03 14:40:20 +0200

    dashboard

--- novelplanner/src/app/app.module.ts
+++ novelplanner/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterComponent } from './character/character.component';
 import {HttpClientModule} from "@angular/common/http";
+import {DashboardComponent} from "./dashboards/dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     CharacterComponent
   ],
   imports: [
